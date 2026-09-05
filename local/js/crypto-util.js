15343d5fc5cfb20e6c2788df7d4c0211e232190f
jd-commit-us2 <yokpem01@gmail.com>
2026-09-06 08:51:45 +0900

    ㄱㄱ

--- local/js/crypto-util.js
+++ local/js/crypto-util.js
@@ -1,7 +1,7 @@
 // js/crypto-util.js
 const WeddingCrypto = {
     DEFAULT_KEY: "__ENCRYPTION_KEY__",
-    SALT: "__SALT__",
+    SALT: new TextEncoder().encode("__SALT__"), // 🌟 이 부분을 다시 TextEncoder로 감싸주세요
 
 
     async deriveKey(passphrase) {
